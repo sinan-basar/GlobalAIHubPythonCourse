2c135fdeb90dd7d328fef61db301961c2a6aba85
snnbsr <53953854+snnbsr@users.noreply.github.com>
2021-02-05 05:28:46 +0300

    Update HW4.py

--- Homeworks/HW4/HW4.py
+++ Homeworks/HW4/HW4.py
@@ -2,15 +2,13 @@
 lower_lim = 0
 upper_lim = 100
 
-if lower_lim < 0:
-    lower_lim=0
+if lower_lim < 2:
+    lower_lim=2
 
-if upper_lim < 1:
-    upper_lim = 1
+if upper_lim < 2:
+    upper_lim = 2
     
 for i in range(lower_lim,upper_lim+1):
-    if i==0:
-        continue
     isPrime = True
     for k in range(2,i):
         if (i%k) == 0:
